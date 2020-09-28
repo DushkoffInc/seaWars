9ea3f84e803c23b1e9c252245138671735c48ce6
Pchelintsev <pchelintsev@teamidea.ru>
2020-09-29 01:05:27 +0300

    убрал лишнее из hello controller

--- src/main/java/dushkof/seaWars/controllers/HelloController.java
+++ src/main/java/dushkof/seaWars/controllers/HelloController.java
@@ -4,35 +4,21 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import dushkof.seaWars.objects.ValCurs;
 import dushkof.seaWars.services.GameService;
 import dushkof.seaWars.services.UserService;
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.annotation.Resource;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
-import java.io.StringReader;
-import java.net.URI;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 @RestController
@@ -68,47 +54,22 @@ public class HelloController {
 
 
     @GetMapping("/t")
-    public String t(){
+    public String t() {
         String publicURI = "http://www.cbr.ru/scripts/XML_daily.asp";
-        HttpGet request = new HttpGet(publicURI);
-        try (CloseableHttpResponse response = httpClient.execute(request)) {
+        try {
+            URL url = new URL(publicURI);
+            JAXBContext jaxbContext = JAXBContext.newInstance(ValCurs.class);
 
-            // Get HttpResponse Status
-            System.out.println(response.getStatusLine().toString());
-
-            HttpEntity entity = response.getEntity();
-            Header headers = entity.getContentType();
-            System.out.println(headers);
-
-            if (entity != null) {
-                String result = EntityUtils.toString(entity);
-// тут начинается магия XD
-                URL url = new URL(publicURI);
-                JAXBContext jaxbContext = JAXBContext.newInstance(ValCurs.class);
-
-                Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-                ValCurs resp = (ValCurs) jaxbUnmarshaller.unmarshal(url);
-                System.out.println(resp.toString());
-                // тут она заканчивается)
-
-                return result;
-            }
-
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JAXBException e) {
-            e.printStackTrace();
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            ValCurs resp = (ValCurs) jaxbUnmarshaller.unmarshal(url);
+            System.out.println(resp.toString());
+            return "см консоль";
+        } catch (MalformedURLException malformedURLException) {
+            malformedURLException.printStackTrace();
+        } catch (JAXBException jaxbException) {
+            jaxbException.printStackTrace();
         }
-
         return "zalupa";
     }
-
-    private URI getURI(String host) {
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(host);
-        return builder.build().encode().toUri();
-    }
-
 
 }
